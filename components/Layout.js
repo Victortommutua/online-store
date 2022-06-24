ed0685db88359994bb592c86c988fda43561eb52
victortommutua <mutuavictor801@gmail.com>
2022-06-25 01:05:41 +0300

    update

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,15 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import React from 'react'
 
-function Layout() {
+function Layout({title, children}) {
   return (
+    <>
+          <Head>
+        <title>{title ? title + 'Online | Store' : 'Online | Store'}</title>
+        <meta name="description" content="Your Favorite online shopping store" />
+        <link rel="icon" href="/logo.svg"/>
+      </Head>
     <div className='flex min-h-screen flex-col justify-between'>
         <header>
             <nav className='flex h-12 justify-between items-center px-4 shadow-md'>
@@ -15,11 +22,12 @@ function Layout() {
                 </div>
             </nav>
         </header>
-        <main className='container m-auto mt-4 px-4'>Home</main>
+        <main className='container m-auto mt-4 px-4'>{children}</main>
         <footer className='flex h-10 justify-center items-center shadow-inner'>
             <p>Copyright © 2022 Online store</p>
         </footer>
     </div>
+    </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Layout from '../components/Layout'
+import ProductItem from '../components/ProductItem'
+import data from '../utils/data'
 
 export default function Home({title}) {
   return (
@@ -9,7 +11,13 @@ export default function Home({title}) {
         <meta name="description" content="Your Favorite online shopping store" />
         <link rel="icon" href="/logo.svg"/>
       </Head>
-      <Layout/>
+      <Layout>
+        <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
+          {data.products.map((product)=> (
+            <ProductItem product={product} key={product.slug}/>
+          ))}
+        </div>
+      </Layout>
     </div>
-  )
+  );
 }
