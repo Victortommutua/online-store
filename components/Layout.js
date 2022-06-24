608dd7e11814edfcded89303e626d565b4725d84
victortommutua <mutuavictor801@gmail.com>
2022-06-24 23:42:07 +0300

    Update

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,24 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Layout() {
   return (
-    <div>
-      <h2 className='text-blue-500'>Layout</h2>
+    <div className='flex min-h-screen flex-col justify-between'>
+        <header>
+            <nav className='flex h-12 justify-between items-center px-4 shadow-md'>
+                <Link href='/'>
+                    <a className='text-lg font-bold'>online store</a>
+                </Link>
+                <div>
+                    <Link href='/cart'><a className='p-2'> Cart </a></Link>
+                    <Link href='/login'><a className='p-2'> Login </a></Link>
+                </div>
+            </nav>
+        </header>
+        <main className='container m-auto mt-4 px-4'>Home</main>
+        <footer className='flex h-10 justify-center items-center shadow-inner'>
+            <p>Copyright © 2022 Online store</p>
+        </footer>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import Layout from '../components/Layout'
 
-export default function Home() {
+export default function Home({title}) {
   return (
     <div>
       <Head>
-        <title>Online | Store</title>
+        <title>{title ? title + 'Online | Store' : 'Online | Store'}</title>
         <meta name="description" content="Your Favorite online shopping store" />
         <link rel="icon" href="/logo.svg"/>
       </Head>
