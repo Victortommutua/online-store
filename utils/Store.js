ee34bbac7f269f7f4d70c0f9cd0ffcc3efd26abf
victortommutua <mutuavictor801@gmail.com>
2022-06-25 05:03:30 +0300

    Update

--- utils/Store.js
+++ utils/Store.js
@@ -1,5 +1,5 @@
 import { createContext, useReducer} from 'react';
-
+import Cookies from 'js-cookie';
 
 
 
@@ -8,7 +8,8 @@ import { createContext, useReducer} from 'react';
 export const Store = createContext();
 
 const initialState = {
-    cart: {cartItems: []},
+    cart: Cookies.get('cart')? JSON.parse(Cookies.get('cart')):
+    {cartItems: []}
 };
 
 function reducer (state, action) {
@@ -22,12 +23,14 @@ function reducer (state, action) {
             item.name === existItem.name ? newItem : item
             )
             : [...state.cart.cartItems, newItem];
-            return {...state, cart: {...state.cart, cartItems}}
+            Cookies.set('cart', JSON.stringify({...state.cart, cartItems }));
+            return {...state, cart: {...state.cart, cartItems}};
         }
         case 'CART_REMOVE_ITEM': {
             const cartItems = state.cart.cartItems.filter(
                 (item)=> item.slug !== action.payload.slug
             );
+            Cookies.set('cart', JSON.stringify({...state.cart, cartItems}))
             return {...state, cart:{...state.cart, cartItems}};
         }
         default:
